b86bff420a652315b6d46374d3e0884698511f15
Dzmitry Shylovich <Dzmitry_Shylovich@epam.com>
2016-07-29 01:07:01 +0300

    feat(body cell): add OnPush strategy

--- src/components/body/BodyCell.ts
+++ src/components/body/BodyCell.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ElementRef, PipeTransform } from '@angular/core';
+import { Component, Input, PipeTransform, HostBinding, ChangeDetectionStrategy } from '@angular/core';
 import { TableColumn } from '../../models/TableColumn';
 import { deepValueGetter } from '../../utils/deepGetter';
 import { TemplateWrapper } from '../../directives/TemplateWrapper';
@@ -24,22 +24,21 @@ import { TemplateWrapper } from '../../directives/TemplateWrapper';
     '[style.width]': 'column.width + "px"',
     '[style.height]': 'column.height + "px"'
   },
-  directives: [ TemplateWrapper ]
+  directives: [ TemplateWrapper ],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DataTableBodyCell {
 
   @Input() column: TableColumn;
   @Input() row: any;
 
+  @HostBinding('class.datatable-body-cell') isBodySell: boolean = true;
+
   get value() {
     if (!this.row) return '';
     const prop: any = deepValueGetter(this.row, this.column.prop);
     const userPipe: PipeTransform = this.column.pipe;
     return userPipe ? userPipe.transform(prop) : prop;
-  }
-
-  constructor(elm: ElementRef) {
-    elm.nativeElement.classList.add('datatable-body-cell');
   }
 
 }
