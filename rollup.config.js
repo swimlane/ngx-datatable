e6017a0e079d602cbade051b5cc5234448a35787
Austin McDaniel <amcdaniel2@gmail.com>
2016-08-15 21:03:25 -0400

    (chore): roll up config updates and removing banner dep in favor of roll up banner

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,14 @@
 var typescript = require('rollup-plugin-typescript');
 var sourcemaps = require('rollup-plugin-sourcemaps');
 var sass = require('rollup-plugin-sass');
+var pkg = require('./package.json');
+
+var banner =
+`/**
+ * angular2-data-table v${pkg.version} (https://github.com/swimlane/angular2-data-table)
+ * Copyright 2016
+ * Licensed under MIT
+ */`;
 
 module.exports = {
   entry: './src/angular2-data-table.ts',
@@ -10,8 +18,11 @@ module.exports = {
   moduleId: 'angular2-data-table',
   moduleName: 'angular2DataTable',
 
+  banner: banner,
+
   external: [
 		'typescript',
+    'core-js',
     '@angular/core',
     '@angular/common',
     '@angular/core',
