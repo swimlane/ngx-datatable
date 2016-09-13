147f3bba191a7a2e9e3a30ca748d7ffd0c063ef6
Austin McDaniel <amcdaniel2@gmail.com>
2016-09-13 08:24:38 -0400

    (release): 0.5.0

--- release/angular2-data-table.es.js
+++ release/angular2-data-table.es.js
@@ -1,5 +1,5 @@
 /**
- * angular2-data-table v0.4.0 (https://github.com/swimlane/angular2-data-table)
+ * angular2-data-table v0.5.0 (https://github.com/swimlane/angular2-data-table)
  * Copyright 2016
  * Licensed under MIT
  */
