f19f5bf38f04327bbdc14262fe430b8fbc35a87f
Austin <amcdaniel2@gmail.com>
2016-11-19 09:33:53 -0500

    (enhancement): add rxjs support!

---
+++ demo/basic/rx.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+
+@Component({
+  selector: 'rx-demo',
+  template: `
+    <div>
+      <h3>Fix Row Height</h3>
+      <datatable
+        class="material striped"
+        [rows]="rows"
+        [columns]="columns"
+        [columnMode]="'force'"
+        [headerHeight]="50"
+        [footerHeight]="50"
+        [rowHeight]="50">
+      </datatable>
+    </div>
+  `
+})
+export class RxDemoComponent {
+
+  rows: Observable<any[]>;
+  
+  columns = [
+    { prop: 'Organization' },
+    { prop: 'DateAdded' },
+    { name: 'Tags' }
+  ];
+
+  constructor() {
+    this.rows = Observable.create((subscriber) => {
+      this.fetch((data) => {
+        subscriber.next(data.splice(0, 15));
+        subscriber.next(data.splice(15, 30));
+        subscriber.complete();
+      });
+    });
+
+    // Rx.DOM.ajax({ url: '/products', responseType: 'json'}).subscribe()
+    // this.rows = Observable.from(rows);
+  }
+
+  fetch(cb) {
+    const req = new XMLHttpRequest();
+    req.open('GET', `assets/data/security.json`);
+
+    req.onload = () => {
+      cb(JSON.parse(req.response));
+    };
+
+    req.send();
+  }
+
+}
