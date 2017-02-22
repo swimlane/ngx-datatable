56d41d074d0a6634bfb5337472e07967983e7da3
Ritik Rishu <ritik.rishu@hotcocoasoftware.com>
2017-02-22 17:20:05 +0530

    unit test for changes

--- src/components/datatable.component.spec.ts
+++ src/components/datatable.component.spec.ts
@@ -13,6 +13,77 @@ describe('Datatable component', () => {
     TestBed.compileComponents();
   }));
 
+  describe('When data for rows change', () => {
+    it('should not alter offset if there is enough data to display in table', () => {
+      let fixture = TestBed.createComponent(DatatableComponent);
+      let initialRows = [
+        { id: 1 },
+        { id: 2 },
+        { id: 3 },
+        { id: 4 },
+        { id: 5 },
+        { id: 6 },
+        { id: 7 },
+      ];
+      let columns = [
+        {
+          prop: 'foo'
+        }
+      ];
+      fixture.componentInstance.rows = initialRows;
+      fixture.componentInstance.columns = columns;
+      fixture.componentInstance.limit = 1;
+
+      fixture.detectChanges();
+
+      expect(fixture.componentInstance.offset).toBe(0);
+
+      fixture.componentInstance.offset = 5;
+      fixture.componentInstance.rows = initialRows.filter((r) => r.id < 6);
+
+      fixture.detectChanges();
+
+      expect(fixture.componentInstance.offset).toBe(5);
+    });
+
+    it('should reduce offset to minimum number that can display table data', () => {
+      let fixture = TestBed.createComponent(DatatableComponent);
+      let initialRows = [
+        { id: 1 },
+        { id: 2 },
+        { id: 3 },
+        { id: 4 },
+        { id: 5 },
+        { id: 6 },
+        { id: 7 },
+      ];
+      let columns = [
+        {
+          prop: 'foo'
+        }
+      ];
+
+      fixture.componentInstance.rows = initialRows;
+      fixture.componentInstance.columns = columns;
+      fixture.componentInstance.limit = 1;
+
+      fixture.detectChanges();
+
+      expect(fixture.componentInstance.offset).toBe(0);
+
+      fixture.componentInstance.offset = 5;
+
+      fixture.detectChanges();
+
+      expect(fixture.componentInstance.offset).toBe(5);
+
+      fixture.componentInstance.rows = initialRows.filter((r) => r.id < 3);
+
+      fixture.detectChanges();
+
+      expect(fixture.componentInstance.offset).toBe(2);
+    });
+  });
   describe('When the column is sorted with a custom comparator', () => {
 
     it('should return a new array', () => {
