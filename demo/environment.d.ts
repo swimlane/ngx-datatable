7993b9c01e6ba2c4e109df4b7315e7f9c56c6853
Austin McDaniel <amcdaniel2@gmail.com>
2017-04-07 10:47:33 -0600

    (deploy): CI

---
+++ demo/environment.d.ts
@@ -0,0 +1,1 @@
+export declare const decorateModuleRef: <T>(value: T) => T;
--- demo/polyfills.d.ts
+++ demo/polyfills.d.ts
@@ -1,6 +1,8 @@
 import 'core-js/es6';
 import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
+import '@angular/platform-browser';
+import '@angular/core';
 import '@angular/platform-browser-dynamic';
 import '@angular/common';
 import 'rxjs/Rx';
