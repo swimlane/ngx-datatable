7662bfac74d7b8a06effa0a6b36a31c15ca9df8e
Dale Lotts <dale.lotts@veritas.com>
2017-05-25 15:49:56 -0500

    docs: remove notice

--- src/components/body/selection.component.ts
+++ src/components/body/selection.component.ts
@@ -1,22 +1,3 @@
-/*
- * VERITAS: Copyright (c) 2017 Veritas Technologies LLC. All rights reserved.
- *
- * THIS SOFTWARE CONTAINS CONFIDENTIAL INFORMATION AND TRADE SECRETS OF VERITAS
- * TECHNOLOGIES LLC. USE, DISCLOSURE OR REPRODUCTION IS PROHIBITED WITHOUT THE PRIOR
- * EXPRESS WRITTEN PERMISSION OF VERITAS TECHNOLOGIES LLC.
- *
- * The Licensed Software and Documentation are deemed to be commercial computer
- * software as defined in FAR 12.212 and subject to restricted rights as defined
- * in FAR Section 52.227-19 "Commercial Computer Software - Restricted Rights"
- * and DFARS 227.7202, Rights in "Commercial Computer Software or Commercial
- * Computer Software Documentation," as applicable, and any successor regulations,
- * whether delivered by Veritas as on premises or hosted services. Any use,
- * modification, reproduction release, performance, display or disclosure of
- * the Licensed Software and Documentation by the U.S. Government shall be
- * solely in accordance with the terms of this Agreement.
-
- * SS45-8248-1773-66-15-0
- */
 import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { SelectionType } from "../../types/selection.type";
 import { Keys } from "../../utils/keys";
