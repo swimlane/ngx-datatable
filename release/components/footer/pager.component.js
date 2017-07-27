fc0e7f71f0bc3f029939d758c710919803e159d8
Van Pham Hoai <hoaivan@gmail.com>
2017-07-27 19:34:23 +0700

    rebuild

--- release/components/footer/pager.component.js
+++ release/components/footer/pager.component.js
@@ -77,8 +77,16 @@ var DataTablePagerComponent = (function () {
         var isMaxSized = maxSize < this.totalPages;
         page = page || this.page;
         if (isMaxSized) {
-            startPage = ((Math.ceil(page / maxSize) - 1) * maxSize) + 1;
-            endPage = Math.min(startPage + maxSize - 1, this.totalPages);
+            startPage = page - Math.floor(maxSize / 2);
+            endPage = page + Math.floor(maxSize / 2), this.totalPages;
+            if (startPage < 1) {
+                startPage = 1;
+                endPage = Math.max(startPage + maxSize - 1, this.totalPages);
+            }
+            else if (endPage > this.totalPages) {
+                startPage = Math.max(this.totalPages - maxSize + 1, 1);
+                endPage = this.totalPages;
+            }
         }
         for (var num = startPage; num <= endPage; num++) {
             pages.push({
