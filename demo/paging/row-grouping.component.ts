6f2b5399dde204c0e32b3da497a7f11fc553ec51
Gus Cunha <cunhag_dev@ihcdev.co.nz>
2017-08-23 09:42:23 +1200

    Fixed TSLint in the RowGrouping Demo

--- demo/paging/row-grouping.component.ts
+++ demo/paging/row-grouping.component.ts
@@ -27,12 +27,34 @@ import { NgStyle } from '@angular/common';
         [limit]="4">
 
         <ngx-datatable-column name="Exp. Pay." prop="" editable="true" frozenLeft="True">
-          <ng-template ngx-datatable-cell-template let-rowIndex="rowIndex" let-value="value" let-row="row" let-group="group">
-              <input type="checkbox" id="ep1{{rowIndex}}" name="{{rowIndex}}" value="0" class="expectedpayment" (change)="checkGroup($event, row, rowIndex, group)" [checked]="row.exppayyes===1">
+          <ng-template ngx-datatable-cell-template let-rowIndex="rowIndex" let-value="value" let-row="row" 
+          let-group="group">
+              <input 
+                type="checkbox" 
+                id="ep1{{rowIndex}}" 
+                name="{{rowIndex}}" 
+                value="0" 
+                class="expectedpayment" 
+                (change)="checkGroup($event, row, rowIndex, group)" 
+                [checked]="row.exppayyes===1">
               <label for="ep1{{rowIndex}}"></label>
-              <input type="checkbox" id="ep2{{rowIndex}}" name="{{rowIndex}}" value="1" class="expectedpayment2" (change)="checkGroup($event, row, rowIndex, group)" [checked]="row.exppayno===1">
+              <input 
+                type="checkbox" 
+                id="ep2{{rowIndex}}" 
+                name="{{rowIndex}}" 
+                value="1" 
+                class="expectedpayment2" 
+                (change)="checkGroup($event, row, rowIndex, group)" 
+                [checked]="row.exppayno===1">
               <label for="ep2{{rowIndex}}"></label>
-              <input type="checkbox" id="ep3{{rowIndex}}" name="{{rowIndex}}" value="2" class="expectedpayment3" (change)="checkGroup($event, row, rowIndex, group)" [checked]="row.exppaypending===1">
+              <input 
+                type="checkbox" 
+                id="ep3{{rowIndex}}" 
+                name="{{rowIndex}}" 
+                value="2" 
+                class="expectedpayment3" 
+                (change)="checkGroup($event, row, rowIndex, group)" 
+                [checked]="row.exppaypending===1">
               <label for="ep3{{rowIndex}}"></label>
           </ng-template>                    
         </ngx-datatable-column>
@@ -42,7 +64,8 @@ import { NgStyle } from '@angular/common';
         <ngx-datatable-column name="Gender" prop="gender"></ngx-datatable-column>
         <ngx-datatable-column name="Age" prop="age"></ngx-datatable-column>
         <ngx-datatable-column name="Comment" prop="comment">
-          <ng-template ngx-datatable-cell-template let-rowIndex="rowIndex" let-value="value" let-row="row" let-group="group" let-rowHeight="rowHeight">           
+          <ng-template ngx-datatable-cell-template let-rowIndex="rowIndex" let-value="value" let-row="row" 
+          let-group="group" let-rowHeight="rowHeight">           
             <input autofocus
               (blur)="updateValue($event, 'comment', rowIndex)"
               type="text" 
@@ -51,7 +74,8 @@ import { NgStyle } from '@angular/common';
           </ng-template>                
         </ngx-datatable-column>
         <ngx-datatable-column name="Group Comment" prop="groupcomment" isGroup="true">
-          <ng-template ngx-datatable-cell-template let-rowIndex="rowIndex" let-value="value" let-row="row" let-group="group" let-rowHeight="rowHeight">           
+          <ng-template ngx-datatable-cell-template let-rowIndex="rowIndex" let-value="value" let-row="row" 
+          let-group="group" let-rowHeight="rowHeight">           
             <textarea 
               autofocus
               [ngStyle]="getGroupRowHeight(group, rowHeight)"
@@ -80,10 +104,10 @@ import { NgStyle } from '@angular/common';
 })
 export class RowGroupingComponent {
 
-  funder = []
-  calculated = []
-  pending = []
-  groups = []
+  funder = [];
+  calculated = [];
+  pending = [];
+  groups = [];
   
   editing = {};  
   rows = [];
@@ -105,89 +129,92 @@ export class RowGroupingComponent {
     req.send();
   }
 
-  getGroupRowHeight(group, rowHeight){
-    var style={};
+  getGroupRowHeight(group, rowHeight) {
+    let style = {};
 
     style = {
-              height: (group.length*40) + 'px',
-              width: '100%'}
+              height: (group.length * 40) + 'px',
+              width: '100%'
+            };
 
     return style;
   }
 
-  checkGroup(event, row, rowIndex, group){
-      var groupStatus: string = "Pending";
-      var expectedPaymentDealtWith:boolean = true;
+  checkGroup(event, row, rowIndex, group) {
+    let groupStatus: string = 'Pending';
+    let expectedPaymentDealtWith: boolean = true;
 
-      row.exppayyes=0
-      row.exppayno=0
-      row.exppaypending=0
-        
-      if (event.target.checked)
-        if (event.target.value==0){ //expected payment yes selected
-          row.exppayyes=1
-        }
-        else if (event.target.value==1){ //expected payment yes selected
-          row.exppayno=1
-        }
-        else if (event.target.value==2){ //expected payment yes selected              
-          row.exppaypending=1
-        }
+    row.exppayyes = 0;
+    row.exppayno = 0;
+    row.exppaypending = 0;
+      
+    if (event.target.checked)
+      if (event.target.value === '0') { // expected payment yes selected
+        row.exppayyes = 1;
+      } else if (event.target.value === '1') { // expected payment yes selected
+        row.exppayno = 1;
+      } else if (event.target.value === '2') { // expected payment yes selected
+        row.exppaypending = 1;
+      }
 
-      if (group.length===2){ //There are only 2 lines in a group
-        if (["Calculated", "Funder"].indexOf(group[0].source)>-1 && ["Calculated", "Funder"].indexOf(group[1].source)>-1){ //Sources are funder and calculated
-          if (group[0].startdate === group[1].startdate && group[0].enddate === group[1].enddate){ //Start dates and end dates match
-            for (var index = 0; index < group.length; index++) {
-              if (group[index].source != row.source)
-              {
-                if (event.target.value==='0'){ //expected payment yes selected
-                  group[index].exppayyes=0;
-                  group[index].exppaypending=0;
-                  group[index].exppayno=1;
-                }
+    if (group.length === 2) { // There are only 2 lines in a group
+      // tslint:disable-next-line:max-line-length
+      if (['Calculated', 'Funder'].indexOf(group[0].source) > -1 && ['Calculated', 'Funder'].indexOf(group[1].source) > -1) { // Sources are funder and calculated
+        // tslint:disable-next-line:max-line-length
+        if (group[0].startdate === group[1].startdate && group[0].enddate === group[1].enddate) { // Start dates and end dates match
+          for (let index = 0; index < group.length; index++) {
+            if (group[index].source !== row.source) {
+              if (event.target.value === '0') { // expected payment yes selected
+                group[index].exppayyes = 0;
+                group[index].exppaypending = 0;
+                group[index].exppayno = 1;
               }
-
-              if (group[index].exppayyes === 0 && group[index].exppayno === 0 && group[index].exppaypending === 0)
-                expectedPaymentDealtWith = false;
-              console.log('expectedPaymentDealtWith', expectedPaymentDealtWith);
             }
+
+            if (group[index].exppayyes === 0 && group[index].exppayno === 0 && group[index].exppaypending === 0)
+              expectedPaymentDealtWith = false;
+            console.log('expectedPaymentDealtWith', expectedPaymentDealtWith);
           }
         }
       }
-      else{
-        for (var index = 0; index < group.length; index++) {
-          if (group[index].exppayyes === 0 && group[index].exppayno === 0 && group[index].exppaypending === 0)
-            expectedPaymentDealtWith = false;
-          console.log('expectedPaymentDealtWith', expectedPaymentDealtWith);
-        }      
-      }
+    } else {
+      for (let index = 0; index < group.length; index++) {
+        if (group[index].exppayyes === 0 && group[index].exppayno === 0 && group[index].exppaypending === 0)
+          expectedPaymentDealtWith = false;
+        console.log('expectedPaymentDealtWith', expectedPaymentDealtWith);
+      }      
+    }
 
-      //check if there is a pending selected payment or a row that does not have any expected payment selected
-      if (group.filter(row => row.exppaypending===1).length===0 && group.filter(row => row.exppaypending===0 && row.exppayyes===0 && row.exppayno===0).length===0)
-      //if (expectedPaymentDealtWith)
-      {
-        console.log('expected payment dealt with')
-        //check if can set the group status
-        const numberOfExpPayYes = group.filter(row => row.exppayyes===1).length;
-        const numberOfSourceFunder = group.filter(row => row.exppayyes===1 && row.source==='Funder').length;
-        const numberOfSourceCalculated = group.filter(row => row.exppayyes===1 && row.source==='Calculated').length;
-        const numberOfSourceManual = group.filter(row => row.exppayyes===1 && row.source==='Manual').length;
+    // check if there is a pending selected payment or a row that does not have any expected payment selected
+    if (group.filter(rowFilter => rowFilter.exppaypending === 1).length === 0 
+      && group.filter(rowFilter => rowFilter.exppaypending === 0 
+                      && rowFilter.exppayyes === 0 
+                      && rowFilter.exppayno === 0).length === 0) {
+      console.log('expected payment dealt with');
+      // check if can set the group status
+      const numberOfExpPayYes = group.filter(rowFilter => rowFilter.exppayyes === 1).length;
+      const numberOfSourceFunder = group.filter(rowFilter => rowFilter.exppayyes === 1 
+                                                && rowFilter.source === 'Funder').length;
+      const numberOfSourceCalculated = group.filter(rowFilter => rowFilter.exppayyes === 1 
+                                                    && rowFilter.source === 'Calculated').length;
+      const numberOfSourceManual = group.filter(rowFilter => rowFilter.exppayyes === 1 
+                                                && rowFilter.source === 'Manual').length;
 
-        console.log('numberOfExpPayYes', numberOfExpPayYes)
-        console.log('numberOfSourceFunder', numberOfSourceFunder)
-        console.log('numberOfSourceCalculated', numberOfSourceCalculated)
-        console.log('numberOfSourceManual', numberOfSourceManual)
+      console.log('numberOfExpPayYes', numberOfExpPayYes);
+      console.log('numberOfSourceFunder', numberOfSourceFunder);
+      console.log('numberOfSourceCalculated', numberOfSourceCalculated);
+      console.log('numberOfSourceManual', numberOfSourceManual);
 
-        if (numberOfExpPayYes>0)
-          if (numberOfExpPayYes === numberOfSourceFunder)
-            groupStatus = 'Funder Selected'
-          else if (numberOfExpPayYes === numberOfSourceCalculated)
-            groupStatus = 'Calculated Selected'
-          else if (numberOfExpPayYes === numberOfSourceManual)
-            groupStatus = 'Manual Selected'  
-          else
-            groupStatus = 'Hybrid Selected'
-      }
+      if (numberOfExpPayYes > 0)
+        if (numberOfExpPayYes === numberOfSourceFunder)
+          groupStatus = 'Funder Selected';
+        else if (numberOfExpPayYes === numberOfSourceCalculated)
+          groupStatus = 'Calculated Selected';
+        else if (numberOfExpPayYes === numberOfSourceManual)
+          groupStatus = 'Manual Selected';
+        else
+          groupStatus = 'Hybrid Selected';
+    }
 
     group[0].groupstatus = groupStatus;    
   }
