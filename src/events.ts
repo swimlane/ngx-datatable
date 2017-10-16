f92a733f0e162fb45cc6593c3f1a5275ed5f4089
earlyster <earlyster@gmail.com>
2017-10-16 15:38:47 -0700

    fix ( MouseEvent ) undefined error

--- src/events.ts
+++ src/events.ts
@@ -1,4 +1,6 @@
 declare let global: any;
 
+import { MouseEvent, KeyboardEvent} from './utils/facade/browser';
+
 export const mouseEvent = (global as any).MouseEvent as MouseEvent;
 export const keyboardEvent = (global as any).KeyboardEvent as KeyboardEvent;
