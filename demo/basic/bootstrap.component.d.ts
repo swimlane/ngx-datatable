3a2d72147eabb6bceb22caad84179a4c76233857
Austin McDaniel <amcdaniel2@gmail.com>
2017-11-05 09:42:17 -0600

    (deploy): CI

---
+++ demo/basic/bootstrap.component.d.ts
@@ -0,0 +1,12 @@
+export declare class BootstrapThemeComponent {
+    rows: any[];
+    loadingIndicator: boolean;
+    reorderable: boolean;
+    columns: ({
+        prop: string;
+    } | {
+        name: string;
+    })[];
+    constructor();
+    fetch(cb: any): void;
+}
