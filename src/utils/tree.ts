94bb988c6208f7dd452709114f9d1b11e326f072
Sanborn <sanborn.sen@gmail.com>
2018-01-05 01:00:21 +0530

    The tree arrangement logic updated to a better version

--- src/utils/tree.ts
+++ src/utils/tree.ts
@@ -30,64 +30,76 @@
  * --5    -> 1
  * 7     -> 8
  *
- * TODO: This is a bruteforce solution, need to find a better one
  *
  * @param rows
  *
  */
+
 export function groupRowsByParents(rows: any[], from: string = '', to: string = ''): any[] {
   if (from !== '' && to !== '') {
-    const childrenMap = {};
-    for (let i = 0; i < rows.length; i++) {
-      childrenMap[rows[i][to]] = {
-        children: rows.filter(row => row[from] === rows[i][to]),
-        checked: false
-      };
-    }
-    const allRootItems = rows.filter(row => row[from] === null ||
-                                     typeof(row[from]) === 'undefined');
-    let opArray = rearrange(allRootItems, childrenMap, 0, true, to);
+    const nodeById = {};
+    const l = rows.length;
+    let node: TreeNode | null = null;
 
-    // Now add items to the list whose parent is not present
-    // Setting all of their levels as 0
-    // TODO: This gets added at the end, need to maintain the order
-    opArray = [...opArray, ...rows.filter(row => {
-      return !childrenMap[row[to]].checked;
-    }).map(item => {
-      item.level = 0;
-      return item;
-    })];
-    return opArray;
+    nodeById[0] = new TreeNode(); // that's the root node
+
+    const uniqIDs = rows.reduce((arr, item) => {
+      if (arr.indexOf(item[to]) === -1) {
+        arr.push(item[to]);
+      }
+      return arr;
+    }, []);
+
+    for (let i = 0; i < l; i++) {  // make TreeNode objects for each item
+      nodeById[ rows[i][to] ] = new TreeNode(rows[i]);
+    }
+
+    for (let i = 0; i < l; i++) {  // link all TreeNode objects
+      node = nodeById[ rows[i][to] ];
+      let parent = 0;
+      if (node.row.hasOwnProperty(from) && !!node.row[from] && (uniqIDs.indexOf(node.row[from]) > -1)) {
+        parent = node.row[from];
+      }
+      node.parent = nodeById[ parent ];
+      node.row['level'] = node.parent.row['level'] + 1;
+      node.parent.children.push(node);
+    }
+
+    let resolvedRows = [];
+    nodeById[0].flatten(function() {
+      resolvedRows = [...resolvedRows, this.row];
+    }, true);
+
+    return resolvedRows;
   } else {
     return rows;
   }
-
 }
 
-function rearrange(items: any[],
-                   map: object,
-                   level: number,
-                   show: boolean = true,
-                   to: string = ''): any[] {
-  if (!items.length) return [];
-  let op: any[] = [];
-  items.forEach(item => {
-    map[item[to]].checked = true;
-    item.level = level;
-    if (show) {
-      op = [
-        ...op,
-        item,
-        ...rearrange(map[item[to]].children,
-                     map,
-                     level + 1,
-                     item.treeStatus === 'expanded',
-                     to)
-      ];
-    } else {
-      op = [...op];
-      rearrange(map[item[to]].children, map, level + 1, false, to);
+class TreeNode {
+  public row: any;
+  public parent: any;
+  public children: any[];
+
+  constructor(row: any | null = null) {
+    if (!row) {
+      row = {
+        level: -1,
+        treeStatus: 'expanded'
+      };
     }
-  });
-  return op;
+    this.row      = row;
+    this.parent   = null;
+    this.children = [];
+  }
+
+  flatten(f, recursive)  {
+    if (this.row['treeStatus'] === 'expanded') {
+      for (let i = 0, l = this.children.length; i < l; i++) {
+        const child = this.children[i];
+        f.apply(child, Array.prototype.slice.call(arguments, 2));
+        if (recursive) child.flatten.apply(child, arguments);
+      }
+    }
+  }
 }
