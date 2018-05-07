efd9a87be88a39b4bed4434ed5984ad3d01123d9
Marjan Georgiev <marjan.georgiev@gmail.com>
2018-05-07 11:18:02 +0200

    (release): 12.0.0

--- release/index.js
+++ release/index.js
@@ -1,5 +1,5 @@
 /**
- * angular2-data-table v"11.3.2" (https://github.com/swimlane/angular2-data-table)
+ * angular2-data-table v"12.0.0" (https://github.com/swimlane/angular2-data-table)
  * Copyright 2016
  * Licensed under MIT
  */
