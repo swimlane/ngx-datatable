388508da1b9c20132dffdbb3382fa1d74f3cd01f
Samuel Ayvazyan <samuel.ayvazyan.fd@gmail.com>
2018-07-19 18:46:26 +0400

    Add canShowDataTable getter/setter to show/hide datatable element.

--- src/components/datatable.component.ts
+++ src/components/datatable.component.ts
@@ -19,12 +19,12 @@ import { DatatableRowDetailDirective } from './row-detail';
 import { DatatableFooterDirective } from './footer';
 import { DataTableHeaderComponent } from './header';
 import { MouseEvent } from '../events';
-import { BehaviorSubject, Subscription } from 'rxjs';
+import { BehaviorSubject, Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'ngx-datatable',
   template: `
-    <div
+    <div *ngIf="canShowDataTable | async"
       visibilityObserver
       (visible)="recalculate()">
       <datatable-header
@@ -655,6 +655,20 @@ export class DatatableComponent implements OnInit, DoCheck, AfterViewInit {
     return this.selected && this.rows &&
       this.rows.length !== 0 && allRowsSelected;
   }
+
+  /**
+   * Returns if datatable is set to shown.
+   */
+  get canShowDataTable(): Observable<boolean> {
+    return this._canShowDataTable.asObservable();
+  }
+  /**
+   * Set if datatable can be shown.
+   */
+  setCanShowDataTable(val: boolean): void {
+    return this._canShowDataTable.next(val);
+  }
+  private _canShowDataTable = new BehaviorSubject<boolean>(true);
 
   element: HTMLElement;
   _innerWidth: number;
