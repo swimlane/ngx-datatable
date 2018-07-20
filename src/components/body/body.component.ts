83525a2d5eb50a367a281d2361b3131f34b6cc0c
Samuel Ayvazyan <samuel.ayvazyan.fd@gmail.com>
2018-07-20 16:16:49 +0400

    Fix issue with accessing data of async rows.

--- src/components/body/body.component.ts
+++ src/components/body/body.component.ts
@@ -46,10 +46,10 @@ import { MouseEvent } from '../../events';
           [rowDetail]="rowDetail"
           [groupHeader]="groupHeader"
           [offsetX]="offsetX"
-          [detailRowHeight]="getDetailRowHeight(group[i],i)"
+          [detailRowHeight]="getDetailRowHeight(group && group[i],i)"
           [row]="group"
           [expanded]="getRowExpanded(group)"
-          [rowIndex]="getRowIndex(group[i])"
+          [rowIndex]="getRowIndex(group && group[i])"
           (rowContextmenu)="rowContextmenu.emit($event)">
           <datatable-body-row
             *ngIf="!groupedRows; else groupedRowsTemplate"
