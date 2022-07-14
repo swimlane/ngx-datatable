a32b661175e48ee2a23cb1b3d306467e401eee8c
Timo Wolf <timowolf@users.noreply.github.com>
2022-07-14 15:52:15 +0200

    fix: correct dynamic vertical scolling (#29)

--- projects/swimlane/ngx-datatable/src/lib/components/datatable.component.ts
+++ projects/swimlane/ngx-datatable/src/lib/components/datatable.component.ts
@@ -861,10 +861,10 @@ export class DatatableComponent implements OnInit, DoCheck, AfterViewInit {
         width = width - this.scrollbarHelper.width;
       }
 
-      if (this.headerComponent?.innerWidth !== width) {
+      if (this.headerComponent && this.headerComponent.innerWidth !== width) {
         this.headerComponent.innerWidth = width;
       }
-      if (this.bodyComponent?.innerWidth !== width) {
+      if (this.bodyComponent && this.bodyComponent.innerWidth !== width) {
         this.bodyComponent.innerWidth = width;
         this.bodyComponent.cd.markForCheck();
       }
