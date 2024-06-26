0e224e4d93c07a61fab6cbe48de4bf33e0efcdd3
chintankavathia <chintan.kavathia@gmail.com>
2024-06-26 11:35:03 +0530

    fix(scroll): horizontal scroll with grouped rows (#59)
    Fixes endless horizontal scroll behaviour when using grouped rows.

--- projects/swimlane/ngx-datatable/src/lib/components/body/body-row-wrapper.component.ts
+++ projects/swimlane/ngx-datatable/src/lib/components/body/body-row-wrapper.component.ts
@@ -132,7 +132,7 @@ export class DataTableRowWrapperComponent implements DoCheck, OnInit {
 
     styles['transform'] = 'translate3d(' + this.offsetX + 'px, 0px, 0px)';
     styles['backface-visibility'] = 'hidden';
-    styles['width'] = this.innerWidth;
+    styles['width'] = this.innerWidth + 'px';
 
     return styles;
   }
