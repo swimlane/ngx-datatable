11ccb3282b0b43102443dc719ff247050d034ef5
Leo Viezens <leo.viezens@web.de>
2025-01-16 14:39:17 +0100

    fix: add missing space before headerClass (#138)

--- projects/swimlane/ngx-datatable/src/lib/components/header/header-cell.component.ts
+++ projects/swimlane/ngx-datatable/src/lib/components/header/header-cell.component.ts
@@ -135,7 +135,7 @@ export class DataTableHeaderCellComponent implements OnInit {
         });
 
         if (typeof res === 'string') {
-          cls += res;
+          cls += ' ' + res;
         } else if (typeof res === 'object') {
           const keys = Object.keys(res);
           for (const k of keys) {
