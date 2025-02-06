7b3dbcc89a80eb50ca0c9b7653176687b8b4a543
Stephen Belovarich <steveblue@gmail.com>
2025-02-05 17:17:20 -0800

    fix: ci unit tests

--- projects/swimlane/ngx-datatable/karma.conf.js
+++ projects/swimlane/ngx-datatable/karma.conf.js
@@ -25,13 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    customLaunchers: {
-      ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu']
-      }
-    },
+    browsers: ['Chrome', 'ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true
   });
