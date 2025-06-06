69273900815fe05e1543710ac4a3a57381f34075
Robert Worgul <robert.worgul@scitotec.de>
2025-06-06 19:21:52 +0200

    refactor: remove unnecessary checks
    Remove check for `this._rows` as it's always defined.

--- projects/swimlane/ngx-datatable/src/lib/components/datatable.component.ts
+++ projects/swimlane/ngx-datatable/src/lib/components/datatable.component.ts
@@ -138,7 +138,7 @@ export class DatatableComponent<TRow extends Row = any>
   @Input() set groupRowsBy(val: keyof TRow) {
     if (val) {
       this._groupRowsBy = val;
-      if (this._rows && this._groupRowsBy) {
+      if (this._groupRowsBy) {
         // creates a new array with the data grouped
         this.groupedRows = this.groupArrayBy(this._rows, this._groupRowsBy);
       }
@@ -853,7 +853,7 @@ export class DatatableComponent<TRow extends Row = any>
         optionalGetterForProp(this.treeToRelation)
       );
 
-      if (this._rows && this._groupRowsBy) {
+      if (this._groupRowsBy) {
         // If a column has been specified in _groupRowsBy create a new array with the data grouped by that row
         this.groupedRows = this.groupArrayBy(this._rows, this._groupRowsBy);
       }
