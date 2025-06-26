9c7f1ed9dddd152243f03253f2431196c06de3c8
Mark Coleman <mark.coleman@swimlane.com>
2025-06-26 12:43:15 -0500

    add type cast in math.spec.ts

--- projects/swimlane/ngx-datatable/src/lib/utils/math.spec.ts
+++ projects/swimlane/ngx-datatable/src/lib/utils/math.spec.ts
@@ -1,5 +1,6 @@
 import { adjustColumnWidths, forceFillColumnWidths } from './math';
 import { toInternalColumn } from './column-helper';
+import { TableColumnInternal } from '../types/internal.types';
 
 describe('Math function', () => {
   describe('forceFillColumnWidths', () => {
@@ -11,7 +12,7 @@ describe('Math function', () => {
           { prop: 'email', width: 250, canAutoResize: true }
         ];
 
-        forceFillColumnWidths(columns, 750, 1, true); // Column 2 expanded from 250 to 400
+        forceFillColumnWidths(columns as TableColumnInternal[], 750, 1, true); // Column 2 expanded from 250 to 400
 
         expect(columns[0].width).toBe(250); // Not changed
         expect(columns[1].width).toBe(400);
@@ -27,7 +28,7 @@ describe('Math function', () => {
           { prop: 'email', width: 250, canAutoResize: true }
         ];
 
-        forceFillColumnWidths(columns, 750, 1, true); // Column 2 contracted from 250 to 180
+        forceFillColumnWidths(columns as TableColumnInternal[], 750, 1, true); // Column 2 contracted from 250 to 180
 
         expect(columns[0].width).toBe(250); // Not changed
         expect(columns[1].width).toBe(180);
