4c000967e2c87a022a9e9e3590e5323af4fc8e9c
Jonathan Avendano <jogaj_26@hotmail.com>
2026-05-05 04:53:58 -0600

    lint self closing tags

--- .eslintrc.js
+++ .eslintrc.js
@@ -60,7 +60,7 @@ module.exports = {
         '@angular-eslint/directive-class-suffix': 'off',
         '@angular-eslint/no-output-native': 'off',
         '@angular-eslint/use-component-view-encapsulation': 'off',
-        '@angular-eslint/template/prefer-self-closing-tags': 'off',
+        '@angular-eslint/template/prefer-self-closing-tags': 'error',
         '@typescript-eslint/array-type': 'error',
         '@typescript-eslint/brace-style': 'off',
         '@typescript-eslint/comma-dangle': 'off',
@@ -120,7 +120,7 @@ module.exports = {
       files: ['*.html'],
       extends: ['plugin:@angular-eslint/template/recommended'],
       rules: {
-        '@angular-eslint/template/prefer-self-closing-tags': 'off'
+        '@angular-eslint/template/prefer-self-closing-tags': 'error'
       }
     }
   ]
