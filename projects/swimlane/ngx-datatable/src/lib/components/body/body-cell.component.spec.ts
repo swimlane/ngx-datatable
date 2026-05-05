3cddfd901f9d7265ba2e863ab0c042dc44d7c33e
Jonathan Avendano <jogaj_26@hotmail.com>
2026-05-05 08:36:26 -0600

    Fixing unit test after cherry-pick

--- projects/swimlane/ngx-datatable/src/lib/components/body/body-cell.component.spec.ts
+++ projects/swimlane/ngx-datatable/src/lib/components/body/body-cell.component.spec.ts
@@ -3,26 +3,27 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { toInternalColumn } from '../../utils/column-helper';
 import { numericIndexGetter } from '../../utils/column-prop-getters';
 import { DataTableBodyCellComponent } from './body-cell.component';
+import { ComponentRef } from '@angular/core';
 
 describe('DataTableBodyCellComponent', () => {
   let fixture: ComponentFixture<DataTableBodyCellComponent>;
-  let component: DataTableBodyCellComponent;
+  let component: ComponentRef<DataTableBodyCellComponent>;
 
   beforeEach(waitForAsync(() => {
     fixture = TestBed.createComponent(DataTableBodyCellComponent);
-    component = fixture.componentInstance;
+    component = fixture.componentRef;
   }));
 
   describe('prop tests', () => {
     // verify there wasn't a mistake where the falsey 0 value
     // resulted in a code path for missing column prop
     it('should get value from zero-index prop', () => {
-      component.row = ['Hello'];
+      component.setInput('row', ['Hello']);
       const columns = toInternalColumn([{ name: 'First Column', prop: 0 }]);
       expect(columns[0].$$valueGetter).toBe(numericIndexGetter);
 
-      component.column = columns[0];
-      expect(component.value).toEqual('Hello');
+      component.setInput('column', columns[0]);
+      expect(component.instance.value()).toEqual('Hello');
     });
   });
 });
